20b0b692d12ed1ed92710d452d26b3447866c4a9
ITDevSuccess <raharisontsidiany@gmail.com>
2023-05-31 20:02:06 +0300

    Changer le langue de projet

--- .history/Formation/settings_20230530213523.py
+++ .history/Formation/settings_20230530213523.py
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'fr'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Indian/Antananarivo'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
